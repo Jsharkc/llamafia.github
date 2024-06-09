f66f4e49c428b84d14f6ebe0ab0ec99b26f1b532
liujchang <1773867204@qq.com>
2024-06-09 20:25:40 +0800

    Publishing to gh-pages (publish.sh)

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -12,7 +12,7 @@ mkdir public
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public gh-pages
+git worktree add -B gh-pages public github/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
